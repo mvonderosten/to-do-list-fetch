fb5593fab0e4227d737e4e906edad0edaebbb20c
mvonderosten <markvonderosten@gmail.com>
2020-02-22 01:54:31 +0000

    first commit

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,42 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-export function Card() {
+export function Card(props) {
 	const [list, setlist] = useState([]);
 	const [content, setContent] = useState();
-	const [remove, setRemove] = useState("");
 
 	return (
 		<div className="card">
 			<div className="card-body">
 				<div className="text-center">
-					<input
-						value={content}
-						onChange={e => setContent(e.target.value)}
-						onKeyPress={e => {
-							if (e.key === "Enter") {
-								setlist(list.concat(content));
-								setContent("");
-							}
-						}}
-					/>
-
-					{list.map((item, index) => {
-						return (
-							<div
-								key={index}
-								onClick={() =>
-									setlist(list.filter(e => e !== item))
-								}>
-								<ul>
-									<li>
-										{"I need to "} {item}
-									</li>
-								</ul>
-							</div>
-						);
-					})}
+					<div className="result">
+						{props.one} {props.two}
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 }
+
+Card.propTypes = {
+	one: PropTypes.string,
+	two: PropTypes.bool
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -10,12 +11,77 @@ import { Card } from "../component/card";
 export function Home() {
 	const [list, setlist] = useState([]);
 	const [content, setContent] = useState();
-	const [remove, setRemove] = useState("");
+
+	function getToDo() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/mark", {
+			method: "GET"
+		})
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				setlist(data);
+				console.log("getToDo", data);
+			});
+	}
+
+	function saveToDo(listToSave) {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/mark", {
+			method: "PUT",
+			body: JSON.stringify(listToSave),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				return resp.json();
+			})
+			.then(data => {
+				console.log("saveToDo", data);
+				getToDo();
+			});
+	}
+
+	useEffect(() => {
+		getToDo();
+	}, []);
 
 	return (
 		<>
 			<h1>To Do List</h1>
-			<Card />
+
+			<div className="card-body d-flex justify-content-center">
+				<div>
+					<input
+						value={content}
+						onChange={e => setContent(e.target.value)}
+						onKeyPress={e => {
+							if (e.key === "Enter") {
+								saveToDo(
+									list.concat({
+										label: content,
+										done: false
+									})
+								);
+								setContent("");
+							}
+						}}
+					/>
+					{list.map((item, index) => {
+						return (
+							<div key={index} className="card-whole">
+								<div
+									key={index}
+									onClick={() =>
+										setlist(list.filter(e => e !== item))
+									}>
+									<Card one={item.label} two={item.done} />
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</>
 	);
 }
